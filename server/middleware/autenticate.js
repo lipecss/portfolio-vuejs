5e10d40a7dbe187f5410893a4d7bc079f8947d64
santos <santos@4vnts.com.br>
2023-05-06 00:25:45 -0300

    vercel analytics test

--- server/middleware/autenticate.js
+++ server/middleware/autenticate.js
@@ -1,12 +1,21 @@
 import jwt from 'jsonwebtoken'
 
-const authRoutes = ['/api/posts/', '/api/projects/', '/api/skills']
+const authRoutes = [
+  '/api/posts/',
+  '/api/projects/',
+  '/api/skills'
+]
 const authMethods = ['POST', 'PUT', 'DELETE']
 
 export default defineEventHandler(event => {
   const { method, url, headers } = event.node.req
 
   const apiEndpoint = authRoutes.some(route => url.includes(route))
+  const regex = /\/api\/(posts\/|projects\/|skills\/)/
+
+  const validRoutes = regex.test(url)
+
+  console.log('validRoutes', validRoutes)
 
   if (apiEndpoint && authMethods.includes(method)) {
 
@@ -24,7 +33,7 @@ export default defineEventHandler(event => {
       try {
         jwt.verify(token, process.env.SUPABASE_JWT_SECRET, (error, decoded) => {
           console.log('verify', error)
-          // if (error) return setResponseStatus(event, 401, 'Invalid Token')
+          if (error) return setResponseStatus(event, 401, 'Invalid Token')
 
           event.tokenDecoded = decoded
         })
