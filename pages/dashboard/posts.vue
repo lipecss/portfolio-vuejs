829104fda138e7153751e92bf799176070e39f46
santos <santos@4vnts.com.br>
2023-05-08 13:15:59 -0300

    trying to fix insertBefore error

--- pages/dashboard/posts.vue
+++ pages/dashboard/posts.vue
@@ -1,107 +1,110 @@
 <template>
-  <loading :active.sync="loadingPosts" color="#42b883" :can-cancel="false" :lock-scroll="true"
-    :is-full-page="true" background-color="#000" />
+  <loading :active.sync="loadingPosts" color="#42b883" :can-cancel="false" :lock-scroll="true" :is-full-page="true"
+    background-color="#000" />
 
-  <div class="inner-container my-0">
-    <h2 class="text-3xl">Meus posts</h2>
+  <div v-show="!loadingPosts">
+    <div class="inner-container my-0">
+      <h2 class="text-3xl">Meus posts</h2>
 
-    <div class="flex w-full items-center mb-7">
-      <div class="ml-auto text-g1 text-xs sm:inline-flex items-center">
-        <span class="mr-3">Página {{ currentPage }} de {{ maxPage }}</span>
-        <button
-          class="disabled:border-gray-600 inline-flex mr-2 items-center h-8 w-8 justify-center text-g1 rounded-md shadow border border-gray-200 dark:border-g1 hover:enabled:border-white hover:enabled:text-white py-0"
-          :disabled="currentPage === 1" @click="paginatePOST(currentPage - 1)">
-          <svg class="w-4" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
-            stroke-linejoin="round">
-            <polyline points="15 18 9 12 15 6"></polyline>
-          </svg>
-        </button>
+      <div class="flex w-full items-center mb-7">
+        <div class="ml-auto text-g1 text-xs sm:inline-flex items-center">
+          <span class="mr-3">Página {{ currentPage }} de {{ maxPage }}</span>
+          <button
+            class="disabled:border-gray-600 inline-flex mr-2 items-center h-8 w-8 justify-center text-g1 rounded-md shadow border border-gray-200 dark:border-g1 hover:enabled:border-white hover:enabled:text-white py-0"
+            :disabled="currentPage === 1" @click="paginatePOST(currentPage - 1)">
+            <svg class="w-4" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
+              stroke-linejoin="round">
+              <polyline points="15 18 9 12 15 6"></polyline>
+            </svg>
+          </button>
 
-        <button
-          class="disabled:border-gray-600 inline-flex mr-2 items-center h-8 w-8 justify-center text-g1 rounded-md shadow border border-gray-200 dark:border-g1 hover:enabled:border-white hover:enabled:text-white py-0"
-          :disabled="currentPage >= maxPage" @click="paginatePOST(currentPage + 1)">
-          <svg class="w-4" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
-            stroke-linejoin="round">
-            <polyline points="9 18 15 12 9 6"></polyline>
-          </svg>
-        </button>
+          <button
+            class="disabled:border-gray-600 inline-flex mr-2 items-center h-8 w-8 justify-center text-g1 rounded-md shadow border border-gray-200 dark:border-g1 hover:enabled:border-white hover:enabled:text-white py-0"
+            :disabled="currentPage >= maxPage" @click="paginatePOST(currentPage + 1)">
+            <svg class="w-4" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round"
+              stroke-linejoin="round">
+              <polyline points="9 18 15 12 9 6"></polyline>
+            </svg>
+          </button>
+        </div>
       </div>
+
+      <table class="w-full text-left">
+        <thead>
+          <tr class="text-g1">
+            <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">Titulo</th>
+            <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 md:table-cell">
+              Slug
+            </th>
+            <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800  text-g1">
+              Likes
+            </th>
+            <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">Criado</th>
+            <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800  text-g1">
+              Editado
+            </th>
+            <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800  text-g1">
+              Ação
+            </th>
+          </tr>
+        </thead>
+
+        <tbody class="text-gray-600 dark:text-gray-100">
+          <tr v-for="(post, index) in posts">
+            <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
+              <nuxt-link class="hover:text-g1" :to="`/post/${post.slug}`" target="_blank" :title="post.title">
+                {{ post.title }}
+              </nuxt-link>
+            </td>
+
+            <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell"> {{ post.slug }}
+            </td>
+            <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell"> {{ post.likes
+            }}</td>
+            <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">{{ parseDate(post.created_at) }}
+            </td>
+            <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
+              <div class="flex items-center">
+                <div class="sm:flex flex-col">
+                  {{ spacetime(post.updated_at).format('numeric-uk') }}
+                  <div class="text-gray-400 text-xs">{{ spacetime(post.updated_at).format('time-24') }}</div>
+                </div>
+              </div>
+            </td>
+            <td class="border-b border-gray-200 dark:border-gray-800">
+              <div class="w-full flex justify-evenly content-center">
+                <button class="text-gray-400" @click="editPost(post)">
+                  <span class="hover:text-blue-500" id="edit">
+                    <ClientOnly>
+                      <font-awesome-icon :icon="['fas', 'pencil-alt']" />
+                    </ClientOnly>
+                  </span>
+                </button>
+
+                <button class="text-gray-400" @click="deletePost(post)">
+                  <span class="hover:text-red-500" id="delete">
+                    <ClientOnly>
+                      <font-awesome-icon :icon="['fa', 'trash-alt']" />
+                    </ClientOnly>
+                  </span>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
-    <table class="w-full text-left">
-      <thead>
-        <tr class="text-g1">
-          <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">Titulo</th>
-          <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800 md:table-cell">
-            Slug
-          </th>
-          <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800  text-g1">
-            Likes
-          </th>
-          <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">Criado</th>
-          <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800  text-g1">
-            Editado
-          </th>
-          <th class="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800  text-g1">
-            Ação
-          </th>
-        </tr>
-      </thead>
-
-      <tbody class="text-gray-600 dark:text-gray-100">
-        <tr v-for="(post, index) in posts">
-          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-            <nuxt-link class="hover:text-g1" :to="`/post/${post.slug}`" target="_blank" :title="post.title">
-              {{ post.title }}
-            </nuxt-link>
-          </td>
-
-          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell"> {{ post.slug }}
-          </td>
-          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell"> {{ post.likes
-          }}</td>
-          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">{{ parseDate(post.created_at) }}</td>
-          <td class="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-            <div class="flex items-center">
-              <div class="sm:flex flex-col">
-                {{ spacetime(post.updated_at).format('numeric-uk')}}
-                <div class="text-gray-400 text-xs">{{ spacetime(post.updated_at).format('time-24')}}</div>
-              </div>
-            </div>
-          </td>
-          <td class="border-b border-gray-200 dark:border-gray-800">
-            <div class="w-full flex justify-evenly content-center">
-              <button class="text-gray-400" @click="editPost(post)">
-                <span class="hover:text-blue-500" id="edit">
-                <ClientOnly>
-                  <font-awesome-icon :icon="['fas', 'pencil-alt']" />
-                </ClientOnly>
-                </span>
-              </button>
-
-              <button class="text-gray-400" @click="deletePost(post)">
-                <span class="hover:text-red-500" id="delete">
-                  <ClientOnly>
-                    <font-awesome-icon :icon="['fa', 'trash-alt']" />
-                  </ClientOnly>
-                </span>
-              </button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <ClientOnly>
+      <NewContentModal
+        type="post"
+        :is-active="isEditing"
+        :item-to-edit="selectedItem"
+        @post="createContent"
+        @close="changeIsEditing"
+      /> 
+    </ClientOnly>
   </div>
-  
-  <ClientOnly>
-    <NewContentModal
-      type="post"
-      :is-active="isEditing"
-      :item-to-edit="selectedItem"
-      @post="createContent"
-      @close="changeIsEditing"
-    /> 
-  </ClientOnly>
 </template>
 
 <script setup>
@@ -151,7 +154,7 @@ const paginatePOST = async (page, type) => {
 
   if (!error.value) {
     let newPosts = postData.value.docs
-    
+
     posts.value = newPosts
     postLimit.value = posts.value.length
     currentPage.value = page
@@ -188,7 +191,7 @@ const createContent = async (value) => {
 
   if (!error.value) {
     useNuxtApp().$toast.success('Operação efetuada com sucesso', { theme: 'dark' })
-    
+
     var i = posts.value.findIndex(post => post._id === data.value.post._id)
 
     posts.value[i] = data.value.post
@@ -201,7 +204,7 @@ const deletePost = async (value) => {
   const { _id } = value
 
   if (window.confirm('Tem certeza que deseja executar esta ação?')) {
-      const { data, error } = await useFetch(`/api/posts/${_id}`, {
+    const { data, error } = await useFetch(`/api/posts/${_id}`, {
       method: 'DELETE',
       headers: {
         'x-access-token': token
@@ -217,8 +220,8 @@ const deletePost = async (value) => {
 
 onBeforeMount(() => {
   nextTick(async () => {
-    loadingPosts.value= true
-  
+    loadingPosts.value = true
+
     const { data: postData, error } = await useLazyAsyncData('postData', () => $fetch('/api/posts/paginate'))
 
     if (!error.value) {
